6987d76e8b1ce96b563a940c3609e8274f117259
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-20 03:28:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name=(dog_name) # takes an argument
+  @this_dogs_name = dog_name # arg ass to var 
+  end 
+
+  def name # method returns the var 
+    @this_dogs_name
+  end 
+end 
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
